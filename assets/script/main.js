48325023688b3fe534e94bb45053b2951e90e6e8
Nemanja <manojlovic911@hotmail.com>
2022-03-31 20:22:38 +0200

    Add methods for updating health bars

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,3 +1,8 @@
+// Generates random value
+let getRandomValue = (min, max) => {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
 let app = Vue.createApp({
   data() {
     return {
@@ -5,6 +10,35 @@ let app = Vue.createApp({
       playerHealth: 100,
       monsterHealth: 100,
     };
+  },
+  computed: {
+    monsterBarStyle() {
+      return { width: this.monsterHealth + "%" };
+    },
+    playerBarStyle() {
+      return { width: this.playerHealth + "%" };
+    },
+  },
+  methods: {
+    // Atack Action
+    attackMonster() {
+      // Random num is multiplied by (maxNum - minNum)
+      // Take a floor value of the above expression to round the expression result
+      // add the minimum again
+      const attackValue = getRandomValue(12, 6);
+      // Reduce the monsters' health by the attack value
+      this.monsterHealth -= attackValue;
+      // Monster attacks back
+      this.attackPlayer();
+    },
+    attackPlayer() {
+      // Random num is multiplied by (maxNum - minNum)
+      // Take a floor value of the above expression to round the expression result
+      // add the minimum again
+      const attackValue = getRandomValue(15, 8);
+      // Reduce players' health by the attack value
+      this.playerHealth -= attackValue;
+    },
   },
 });
 
